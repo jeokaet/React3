f3b266879ac3bfa36f9bc0dfbba915a166c03193
HJM <jungmzzang@naver.com>
2025-05-08 17:36:30 +0900

    서비스페이지 구성 수정중

--- src/pages/recommend/RecommendPage.jsx
+++ src/pages/recommend/RecommendPage.jsx
@@ -3,49 +3,33 @@ import StepBar from "./StepBar";
 import Map from "./Map";
 import { Grid, Box } from "@mui/material";
 import MainContent from "./MainContent";
+
+
 function RecommendPage() {
+
+  
   return (
     <Box
       sx={{
-        height: "calc(100vh - 64px)", // 헤더 fixed 고려
-        marginTop: "64px",
+        width:"100%", height: "100%",
+        position: "fixed", 
+        top: "64px",
         display: "flex",
-        flexDirection: "column",
       }}
     >
-      <Grid container sx={{ flex: 1 }}>
-        {/* 왼쪽 사이드바 */}
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{
-            width:"5%",
-            height: { xs: "30vh", md: "100%" },
-            overflowY: "auto",
-            backgroundColor: "#f9f9f9",
-          }}
-        >
-          <StepBar />
+      {/* <Grid container sx={{ flex: 1, height: "100%" }}> */}
+        <Grid item xs={12} md={4.5} sx={{ width:"30%", height: "100%",  borderRight: "1px solid #ccc", display:"flex" }}>
+          <Grid sx={{ width:"20%", height: "100%",  borderRight: "1px solid #ccc" }}>
+            <StepBar/>
+          </Grid>
+          <Grid sx={{ width:"80%", height: "100%",  borderRight: "1px solid #ccc" }}>
+            <MainContent />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={3.5} sx={{width:"10%", height:{ xs: "30vh", md: "100%" }, overflow:"auto",background:"f9f9f9"}}>
-
-        </Grid>
-
-        {/* 오른쪽 지도 */}
-        <Grid
-          item
-          xs={12}
-          md={8}
-          sx={{
-            width:"85%",
-            height: { xs: "70vh", md: "100%" },
-            overflow: "hidden",
-          }}
-        >
+        <Grid item xs={12} md={7.5} sx={{ width:"70%", height: "100%", overflow: "hidden", }}>
           <Map />
         </Grid>
-      </Grid>
+      {/* </Grid> */}
     </Box>
   );
 }
